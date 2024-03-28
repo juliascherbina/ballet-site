5d8dbe7e8e1c04dff850e3a9be88b2d49bccc4cd
Julia Scherbina <juliascherbina15@gmail.com>
2024-03-29 00:54:58 +0200

    icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import '../App.css'
+import photo from '../Photo/photo_2024-02-22_14-59-31 (2).jpg'
+import photo2 from '../Photo/photo_2024-02-22_14-59-31.jpg'
+import photo3 from '../Photo/photo_2024-02-22_14-59-32.jpg'
 
 
 
+export const Header = () => {
 
-export const Header=()=>{
-  
-    return <>
-          <div className="header">
-            <div>
-       
-            </div>
-            <div className="header-items">
-                <p className="header-text">  Relax and Stretch your body with Yuliia </p>
-            </div>
-          </div>
-    </>
+  return <>
+    <div className="header">
+      <div className="header-items">
+        <p className="header-text">  Relax and Stretch your body with Yuliia </p>
+      </div>
+      <img className="header-img" src={photo} />
+      <img className="header-img" src={photo2} />
+      <img className="header-img" src={photo3} />
+    </div>
+  </>
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import '../App.css'
 import { Link } from "react-router-dom";
 
-import { Select } from "./Select";
 
-
+import ballerina from '../Photo/9876466.jpg'
 
 
 
@@ -13,20 +12,23 @@ export const Main = () => {
     return <>
         <div className="main">
             <div className="main-sessions">
-                  <p>Yuliia is a proffessional ballet dancer. With huge experience in training practice. She visited a lot of countries with different ballet companies. 
-                    Her lessons for adults with no experience and with huge experience in body movement. Lessons included stretching, balance, abs, back workouts also with body-ballet
-                     movements. After her lessons you really start to feel relief in your muscles,strength in your body and all your body parts will be thank you for those type of workload.   </p>
+                <p>Yuliia is a proffessional ballet dancer. With huge experience in training practice.
+                    She visited a lot of countries with different ballet companies.
+                    Her lessons for adults with no experience and with huge experience in body movement.
+                    Lessons included stretching, balance, abs, back workouts also with body-ballet
+                    movements. After her lessons you really start to feel relief in your muscles,strength in your
+                    body and all your body parts will be thank you for those type of workload.   </p>
                 <div className="main-select">
-                    <h3 className="main-header">Select your sessions</h3>
+                    <h3 className="main-header"> sessions</h3>
                     <div className="main-item">
-                        <img></img>
+                    <img className="header-img2" src={ballerina}/>
                         <h4>30min</h4>
                         <span>50$</span>
                         <Link to='/pay'><button>select</button></Link>
 
                     </div>
                     <div className="main-item">
-                        <img></img>
+                        <img className="header-img2" src={ballerina}/>
                         <h4>60min</h4>
                         <span>100$</span>
                         <Link to='/pay'><button >select</button>
